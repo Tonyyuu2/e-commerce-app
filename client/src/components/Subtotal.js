f0bdba9d79c13206102c92e5c03dcd99413f0163
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-06-07 16:06:58 -0700

    fixed currency formatter react

--- client/src/components/Subtotal.js
+++ client/src/components/Subtotal.js
@@ -6,18 +6,16 @@ function Subtotal() {
   return (
     <div className="subtotal">
       <CurrencyFormat
-        renderText={(value) => {
+        renderText={(value) => (
           <>
             <p>
-              Subtotal ( 0 items):
-              <strong>0</strong>
+              Subtotal (0 items): <strong>0</strong>
             </p>
             <small className="subtotal__gift">
-              <input type="checkbox" />
-              This order contains a gift
+              <input type="checkbox" /> This order contains a gift
             </small>
-          </>;
-        }}
+          </>
+        )}
         decimalScale={2}
         value={0}
         displayType={"text"}
